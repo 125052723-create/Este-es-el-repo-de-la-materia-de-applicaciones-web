9ca66e72c3b60e6d9800426fd9f03e0e7aaf63aa
125052723-create <125052723@upq.edu.mx>
2026-09-17 15:31:42 -0600

    Commit de PHP

---
+++ index.php
@@ -0,0 +1,34 @@
+<!-- <?php
+    echo "Hola Mundo!";
+?> -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+
+    <style>
+        h1{
+            color: #b65df1;
+        }
+
+
+    </style>
+
+</head>
+
+
+<body>
+    
+   <h1>Hola Mundo</h1>
+<h1> Hola Mundo </h1>
+<h2> Hola Mundo </h2>
+<h3> Hola Mundo </h3>
+   <h4> Hola Mundo </h4>
+
+
+</body>
+</html>
