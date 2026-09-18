5ae865fd7bb22805ee15edfab0eb6fa7132a1db0
125052723-create <125052723@upq.edu.mx>
2026-09-18 17:23:21 -0600

    Agregamos dos divisiones

--- index.php
+++ index.php
@@ -14,6 +14,24 @@
         h1{
             color: #b65df1;
         }
+        .seccion{
+            background-color: lightgreen;
+            padding: 20px 40px;
+            margin: 20px;
+            border: 10px solid black
+        }
+        .seccion2{
+            background-color: red;
+            padding-top: 20px;
+            padding-right: 40px;
+            padding-bottom: 20px;
+            padding-left: 40px;
+
+            margin-top: 20px;
+            margin-right: 20px;
+            margin-bottom: 20px;
+            margin-left: 20px;
+        }
 
 
     </style>
@@ -28,6 +46,31 @@
 <h2> Hola Mundo </h2>
 <h3> Hola Mundo </h3>
    <h4> Hola Mundo </h4>
+
+   <div class="seccion"> 
+
+    <h2> Titulo del parrafo </h2>
+
+    <p> Lorem ipsum dolor sit amet consectetur, 
+        adipisicing elit. Quam optio, dolorum debitis facilis tempora quas
+        quisquam totam ullam reprehenderit animi quod ad atque? 
+        Mollitia veniam aliquam, perspiciatis ullam alias nostrum!
+    </p>
+
+   </div>
+
+   <div class="seccion2">
+    <h2>Titulo del parrafo</h2>
+
+    <p>Lorem.ipsum.dolor.sit.amet, consectetur .adipiscing.elit,
+    sed. do .eiusmod.tempor . incididunt.ut .labore .et .dolore.magna aliqua.
+    Ut.enim.ad.minim.veniam, quis .nostrud.exercitation ullamco .laboris.
+    nisi.ut.aliquip.ex.ea.commodo.consequat. .Duis.aute.irure.dolor.in.
+    reprehenderit in.voluptate.velit.esse.cillum dolore.eu.fugiat.nulla.
+    pariatur. Excepteur sint.occaecat .cupidatat.non proident, sunt.in.
+    culpa qui .officia deserunt .mollit.anim.id.est.laborum</p>
+
+    </div>
 
 
 </body>
